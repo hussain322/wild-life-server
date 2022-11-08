8c25fd68784d223358c4ed7b901e5668d02683cf
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-09 00:13:47 +0600

    set basic server

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middle Wares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("wild life server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
