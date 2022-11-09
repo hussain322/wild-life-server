c617fded6bc72e1364f51ee4ab30a0ae877ae399
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-09 20:32:48 +0600

    added post for reviews

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,11 +20,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("wildLife").collection("services");
+    const reviewCollection = client.db("wildLife").collection("reviews");
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
     });
   } finally {
   }
