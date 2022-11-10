56b216e61c207e9b9d782366381aa9e11993d01d
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-10 23:02:15 +0600

    final commit

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "5",
+        expiresIn: "1h",
       });
       res.send({ token });
     });
