1079dbc140362c614fd2920e6942596748093eef
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-10 22:51:16 +0600

    set jwt token

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { query } = require("express");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,6 +19,24 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "unauthorized access" });
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (
+      error,
+      decoded
+    ) {
+      if (error) {
+        res.status(401).send({ message: "unauthorized access" });
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+}
 
 async function run() {
   try {
@@ -52,7 +72,12 @@ async function run() {
     });
 
     //Reviews Api
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log(decoded);
+      if (decoded.email !== req.query.email) {
+        res.status(401).send({ message: "unauthorized access" });
+      }
       let query = {};
       if (req.query.email) {
         query = {
@@ -102,6 +127,15 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const result = await reviewCollection.deleteOne(query);
       res.send(result);
+    });
+
+    //Jwt api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5",
+      });
+      res.send({ token });
     });
   } finally {
   }
